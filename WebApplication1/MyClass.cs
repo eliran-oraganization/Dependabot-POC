861eb72eba333ff87283aabf947e9cf2d29ab481
Eliran Azulay <145766995+eliran-legit@users.noreply.github.com>
2024-06-11 14:11:52 +0300

    FF

--- WebApplication1/MyClass.cs
+++ WebApplication1/MyClass.cs
@@ -1,15 +1,9 @@
 using Legit.Common.Domain;
-using Legit.Common.License.Clients;
+
 
 namespace WebApplication1;
 
 public class MyClass
 {
     public ScmType ScmType = ScmType.Artifactory;
-    public UserManagementConfiguration User;
-
-    public MyClass(UserManagementConfiguration user)
-    {
-        User = user;
-    }
 }
